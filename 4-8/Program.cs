4d8cd35ea283514ac5a3ca91b7727cad2a60abe4
jbg179 <jbg179@naver.com>
2022-12-08 01:41:06 +0900

    4-8

---
+++ 4-8/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class CommandLineArgsApp
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i < args.Length; ++i)
+            Console.WriteLine("Argument[{0}] = {1}", i, args[i]);
+    }
+}
